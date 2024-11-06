12cbf7d9159365921808242ea1701089748669d2
Rijal15 <orepmulyo278@gmail.com>
2024-11-06 13:43:52 +0700

    Update Belajar.dart

--- Belajar.dart
+++ Belajar.dart
@@ -18,13 +18,41 @@ class MyApp extends StatelessWidget {
             builder: (context) {
               return Column(
                 children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
+                  const Text('Flutter', style: TextStyle(fontSize: 100),),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(0, 0, 0, 0),
+                              width: 1.0,
+                              style: BorderStyle.solid
+                            )
+                          ),
+                      hintText: "Email"
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(0, 0, 0, 0),
+                              width: 1.0,
+                              style: BorderStyle.solid
+                            )
+                          ),
+                      hintText: "password"
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  Container(margin: EdgeInsets.symmetric(),),
+                   const Text('forgot password', style: TextStyle(fontSize: 10, color: Color.fromARGB(255, 6, 137, 234)),),
+                   const SizedBox(height: 20,),
                   ElevatedButton(
+                    style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 6, 137, 234))),
                     onPressed: () {
                       print('Click!');
                     },
-                    child: const Text('A button'),
+                    child: const Text('login')
                   ),
                 ],
               );
